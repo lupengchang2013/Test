44b831dc8a8cad76d425246e1d8bf4216299ec68
lpc <lupengchang2013@gmail.com>
2014-06-02 16:44:01 -0400

    initial checking

---
+++ Test/src/test/Test.java
@@ -0,0 +1,17 @@
+package test;
+
+
+import com.lpc.demoJar.sayHello;
+import com.lpc.hello.*;
+
+public class Test {
+	public static void main (String[] args)
+	{
+	
+		Hello hello=new Hello();
+		hello.sayHello("lpc");
+		sayHello sh=new sayHello();
+		sh.greeting("xw");
+	}
+	}
+
