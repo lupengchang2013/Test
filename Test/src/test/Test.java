f5249b40d050bb43ce474f0190dda48feb8e1850
lpc <lupengchang2013@gmail.com>
2014-06-02 21:53:47 -0400

    thrid commit and change

--- Test/src/test/Test.java
+++ Test/src/test/Test.java
@@ -2,15 +2,12 @@ package test;
 
 
 import com.lpc.demoJar.sayHello;
-import com.lpc.hello.*;
 
 public class Test {
 	public static void main (String[] args)
 	{
 	
 		//lpc made changes
-		Hello hello=new Hello();
-		hello.sayHello("lpc");
 		sayHello sh=new sayHello();
 		sh.greeting("xw");
 	}
